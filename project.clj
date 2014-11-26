5f3b4980a7bd1fc0cf287f94025c864c0637b81b
Dan Midwood <dan@danmidwood.com>
2014-11-25 22:20:08 -0300

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject expensapp "0.1.0"
+(defproject expensapp "0.1.1-SNAPSHOT"
   :description "Expensapp: An expense tracking web app"
   :url "http://github.com/danmidwood/expensapp"
   :license "MIT License"
